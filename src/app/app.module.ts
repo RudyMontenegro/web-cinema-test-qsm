7b645c50f8f4fae35e8b7f7e6800669b605512a8
RudyMontenegro <rudymontenegro4@gmail.com>
2023-03-31 14:29:15 -0400

    first commit with nabar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MoviesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
