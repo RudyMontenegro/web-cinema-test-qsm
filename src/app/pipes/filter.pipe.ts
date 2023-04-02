19eb8153132b7d50175be11f1fd035d195331d67
RudyMontenegro <rudymontenegro4@gmail.com>
2023-04-02 13:19:51 -0400

    filtro mejorado creado

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,9 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, arg: any)  {
+    if(arg === '' || arg.length < 2 ) return value;
     const resultMovies = [];
     for(const movie of value){
-      if(movie.genre.indexOf(arg) > -1 ){
+      if(movie.genre.toLowerCase().indexOf(arg.toLowerCase()) > -1 ){
         resultMovies.push(movie);
       }
     }
